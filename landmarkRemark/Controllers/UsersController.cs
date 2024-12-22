87976bb27b3b6d8b773309395f55b4faf7febfbd
Mark Dyvan Aquino <markdyvanaquino@gmail.com>
2024-12-22 13:00:00 +0800

    added user management APIs including CRUD operations

---
+++ landmarkRemark/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using landmarkRemark.Models;
+
+namespace landmarkRemark.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private landmarkEntities db = new landmarkEntities();
+
+        // GET: api/Users
+        public IQueryable<User> GetUsers()
+        {
+            return db.Users.OrderBy(x=>x.userName);
+        }
+
+        // GET: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(int id)
+        {
+            return db.Users.Count(e => e.Id == id) > 0;
+        }
+    }
+}
